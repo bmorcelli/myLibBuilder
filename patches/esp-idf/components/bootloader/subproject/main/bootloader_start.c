fd0219551b80844c7b88b6b562aa464be7f23dd8
Pirata <bmorcelli@gmail.com>
2026-07-13 18:28:29 -0300

    start point

---
+++ patches/esp-idf/components/bootloader/subproject/main/bootloader_start.c
@@ -0,0 +1,109 @@
+/*
+ * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdbool.h>
+#include "esp_log.h"
+#include "esp_rom_sys.h"
+#include "bootloader_init.h"
+#include "bootloader_utility.h"
+#include "bootloader_common.h"
+#include "bootloader_hooks.h"
+
+static const char *TAG = "boot";
+
+static int select_partition_number(bootloader_state_t *bs);
+static int selected_boot_partition(const bootloader_state_t *bs);
+
+/*
+ * We arrive here after the ROM bootloader finished loading this second stage bootloader from flash.
+ * The hardware is mostly uninitialized, flash cache is down and the app CPU is in reset.
+ * We do have a stack, so we can do the initialization in C.
+ */
+void __attribute__((noreturn)) call_start_cpu0(void)
+{
+    // (0. Call the before-init hook, if available)
+    if (bootloader_before_init) {
+        bootloader_before_init();
+    }
+
+    // 1. Hardware initialization
+    if (bootloader_init() != ESP_OK) {
+        bootloader_reset();
+    }
+
+    // (1.1 Call the after-init hook, if available)
+    if (bootloader_after_init) {
+        bootloader_after_init();
+    }
+
+#ifdef CONFIG_BOOTLOADER_SKIP_VALIDATE_IN_DEEP_SLEEP
+    // If this boot is a wake up from the deep sleep then go to the short way,
+    // try to load the application which worked before deep sleep.
+    // It skips a lot of checks due to it was done before (while first boot).
+    bootloader_utility_load_boot_image_from_deep_sleep();
+    // If it is not successful try to load an application as usual.
+#endif
+
+    // 2. Select the number of boot partition
+    bootloader_state_t bs = {0};
+    int boot_index = select_partition_number(&bs);
+    if (boot_index == INVALID_INDEX) {
+        bootloader_reset();
+    }
+
+    // 2.1 Load the TEE image
+#if CONFIG_SECURE_ENABLE_TEE
+    bootloader_utility_load_tee_image(&bs);
+#endif
+
+    // 3. Load the app image for booting
+    bootloader_utility_load_boot_image(&bs, boot_index);
+}
+
+// Select the number of boot partition
+static int select_partition_number(bootloader_state_t *bs)
+{
+    // 1. Load partition table
+    if (!bootloader_utility_load_partition_table(bs)) {
+        ESP_LOGE(TAG, "load partition table error!");
+        return INVALID_INDEX;
+    }
+
+    // 2. Select the number of boot partition
+    return selected_boot_partition(bs);
+}
+
+/*
+ * Selects a boot partition.
+ * The conditions for switching to another firmware are checked.
+ */
+static int selected_boot_partition(const bootloader_state_t *bs)
+{
+    int boot_index = bootloader_utility_get_selected_boot_partition(bs);
+    if (boot_index == INVALID_INDEX) {
+        return boot_index; // Unrecoverable failure.
+    }
+
+    // components/soc/esp32p4/include/soc/reset_reasons.h
+    int reset_reason = esp_rom_get_reset_reason(0);
+    if (reset_reason == RESET_REASON_CHIP_POWER_ON || reset_reason == RESET_REASON_CORE_DEEP_SLEEP || reset_reason == RESET_REASON_CORE_MWDT) {
+        if (bs->test.offset != 0) {
+            return TEST_APP_INDEX;
+        }
+        if (bs->factory.offset != 0) {
+            return FACTORY_INDEX;
+        }
+    }
+
+    return boot_index;
+}
+
+#if CONFIG_LIBC_NEWLIB
+// Return global reent struct if any newlib functions are linked to bootloader
+struct _reent *__getreent(void)
+{
+    return _GLOBAL_REENT;
+}
+#endif
